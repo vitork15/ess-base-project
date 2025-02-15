19f9b11970889b5d162d2128bd5504aa042bddbd
Vitor Manoel <vitorearthur15@hotmail.com>
2025-02-15 19:50:03 -0300

    feature(user/recovery): improving user/recovery logic

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,8 +1,9 @@
-import { Repository } from "typeorm";
+import { Repository, MoreThan } from "typeorm";
 import User from "../entities/user.entity";
 import dbConn from "../database/postgresConnection";
 import { createTransport, SentMessageInfo } from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
+import { randomBytes } from "crypto";
 
 
 class UserService{
@@ -14,6 +15,10 @@ class UserService{
 
     async insertUser(name: string, login: string, email: string, password: string, birthday: Date) : Promise<User>{
         let user = new User()
+
+        if(password.length < 6){
+            throw new Error("password size insufficient")
+        }
 
         user.name = name
         user.login = login
@@ -48,6 +53,9 @@ class UserService{
         if(!user){
             throw new Error("user not found")
         }
+        if(password.length < 6){
+            throw new Error("password size insufficient")
+        }
         user.name = name
         user.login = login
         user.password = password
@@ -61,6 +69,13 @@ class UserService{
         if(!user){
             throw new Error("user not found")
         }
+
+        let token = randomBytes(20).toString('hex');
+
+        user.recoverytoken = token;
+        user.recoveryexpire = Date.now() + 300000; // 5 minutos
+
+        await this.userRepository.save(user)
 
         let mailOptions = {
             from: "",
@@ -77,10 +92,27 @@ class UserService{
             },
         })
         
-        let emailsent = null
-        emailsent = await transporter.sendMail(mailOptions)
+        let emailsent = await transporter.sendMail(mailOptions)
 
         return emailsent
+    }
+
+    async changePassword(token: string, password: string, confirm: string) : Promise<User> {
+        let user = await this.userRepository.findOne({where:{recoverytoken: token, recoveryexpire: MoreThan(Date.now())}})
+        if(!user){
+            throw new Error("token invalid or expired")
+        }
+        if(password != confirm){
+            throw new Error("passwords are not the same")
+        }
+        if(password.length < 6){
+            throw new Error("password size insufficient")
+        }
+        user.password = password;
+        user.recoverytoken = null;
+        user.recoveryexpire = null;
+
+        return await this.userRepository.save(user)
     }
 }
 
