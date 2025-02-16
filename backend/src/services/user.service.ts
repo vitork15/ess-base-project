00e366e1b648c053233561305c49a168b62d81e2
Vitor Manoel <vitorearthur15@hotmail.com>
2025-02-16 11:33:57 -0300

    feature(recovery): definindo mensagem de recuperação

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -4,6 +4,7 @@ import dbConn from "../database/postgresConnection";
 import { createTransport, SentMessageInfo } from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 import { randomBytes } from "crypto";
+import { hostname } from "os";
 
 
 class UserService{
@@ -78,10 +79,10 @@ class UserService{
         await this.userRepository.save(user)
 
         let mailOptions = {
-            from: "",
+            from: process.env.EMAIL_USER,
             to: email,
             subject: "Recuperação de conta",
-            text: "Sua senha é: " + user.password
+            html: "<p>Clique <a href=" + "http://" + hostname() + ":" + process.env.PORT + "/users/recovery/" + token + ">aqui</a> para realizar a mudança de senha</p>"
         }
 
         const transporter = createTransport({
