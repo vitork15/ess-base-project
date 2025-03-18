91124c3446b470b135e227a2f55ff2a75cefc241
Vyniciux <leandro.vynicius123@gmail.com>
2025-03-18 18:47:52 -0300

    fix(frontend): Removing the sidebar of App.tsx to keep only the route scheme

--- frontend/src/app/components/sidebar/index.tsx
+++ frontend/src/app/components/sidebar/index.tsx
@@ -1,40 +1,48 @@
+import { useNavigate } from 'react-router-dom';
 import style from './index.module.css';
 
 export default function Sidebar() {
+
+  const navigate = useNavigate();
+  const navigateTo = (path: string) => {
+  navigate(path);
+  }
+  
   return (
-    <div className={style.sidebar}>
-
-      <div className={style.menu}>
-        <button className={style.logo}>
-          <div>Elefante</div>
-        </button>
-        <button className={style.button}>
-          Buscar
-        </button>
-        <button className={style.button}>
-          Perfil
-        </button>
-        <button className={style.button}>
-          Biblioteca
-        </button>
-      </div>
-    
-      <div className={style.player}>
-        <div>
-          <div className={style.cover}>
-
-          </div>
-          <div className={style.songName}>
-            song name
-          </div>
+    <div>
+      <div className={style.sidebar}>
+        <div className={style.menu} >
+          <button className={style.logo} onClick={() => navigateTo('/home')}>
+            <div>Elefante</div>
+          </button>
+          <button className={style.button} >
+            Buscar
+          </button>
+          <button className={style.button}>
+            Perfil
+          </button>
+          <button className={style.button} onClick={() => navigateTo('/biblioteca')}>
+            Biblioteca
+          </button>
         </div>
-        <div className={style.actions}>
-          <button className={style.actionsButton}>&lt;</button>
-          <button className={style.actionsButton}>||</button>
-          <button className={style.actionsButton}>&gt;</button>
-          <button className={style.actionsButton}>&lt;3</button>
-          <button className={style.actionsButton}>@</button>
-          <button className={style.actionsButton}>+</button>
+      
+        <div className={style.player}>
+          <div>
+            <div className={style.cover}>
+
+            </div>
+            <div className={style.songName}>
+              song name
+            </div>
+          </div>
+          <div className={style.actions}>
+            <button className={style.actionsButton}>&lt;</button>
+            <button className={style.actionsButton}>||</button>
+            <button className={style.actionsButton}>&gt;</button>
+            <button className={style.actionsButton}>&lt;3</button>
+            <button className={style.actionsButton}>@</button>
+            <button className={style.actionsButton}>+</button>
+          </div>
         </div>
       </div>
     </div>
