6959c1608f3072f9498ddf6806957f6bda49ae76
LatchedMicrobe0 <rodrigo.pontes.oliveira.lima@gmail.com>
2025-03-23 14:01:28 -0300

    feat(search): Connect Search Page with Search backend API)

--- frontend/src/app/pages/Busca/index.tsx
+++ frontend/src/app/pages/Busca/index.tsx
@@ -1,28 +1,76 @@
-import { useNavigate } from 'react-router-dom';
-import Sidebar from '../../components/sidebar';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './index.module.css';
+import axios from 'axios';
+import { useState } from 'react';
+
+interface ResultModel{
+  name:string
+}
 
 export default function SearchPage() {
 
+  const [toastMessage, setToastMessage] = useState("");
+  const [showToast, setShowToast] = useState(false);
+  const [searchQuery,setSearchQuery] = useState("")
+  const [resultList,setResultList] = useState<ResultModel[]>([])
   const navigate = useNavigate();
-  const navigateTo = (path: string) => {
-    navigate(path);
-  }
 
+  const handleSubmit = async (event) => {
+
+    event.preventDefault();
+    let newURL;
+    
+    if(searchQuery == ""){
+      newURL= `/search`
+    }else{
+      newURL = `/search?ds=${encodeURIComponent(searchQuery)}`
+    }
+   
+    navigate(newURL)
+    try {
+      const response = await fetch("http://localhost:5001"+newURL, {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+      });
+
+      const responseText = await response.text(); // Pega a resposta do servidor
+      const responseData = JSON.parse(responseText);
+      setResultList(responseData)
+
+      if (!response.ok) throw new Error(responseData.error || "Erro desconhe");
+
+      setToastMessage(responseData.message); // Define a mensagem do toast
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 3000);
+      setSearchQuery("")
+
+  } catch (error) {
+      setToastMessage((error as Error).message); // Define a mensagem do toast
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 3000);
+  }  
+  };
+  
+  
   return (
       <main className="content">
 
-        <header className={styles.header}>
+        <form className={styles.header} onSubmit={handleSubmit}>
           <div className={styles.search_container}>
-              <input className={styles.search_container_input} type="text" placeholder="Pesquisar..." />
-              <button className={styles.search_button}>&#128269;</button>
+              <input 
+              className={styles.search_container_input} 
+              value={searchQuery}
+              onChange={(event) => setSearchQuery(event.target.value)} 
+              type="text" 
+              placeholder="Pesquisar..." />
+              <button className={styles.search_button} type="submit">&#128269;</button>
           </div>
-        </header>
+        </form>
 
         <section className={styles.results_header}>
           <h2>Resultados</h2>
           <div className="filter">
-            <button className={styles.filter_button}>Álbum</button>
+            <button className={styles.filter_button}>Música</button>
             <button className={styles.filter_button}>Playlist</button>
             <button className={styles.filter_button}>Artista</button>
           </div>
@@ -30,34 +78,18 @@ export default function SearchPage() {
         
         <section className={styles.result_list}>
           
-          <article className={styles.result_item}>
-            <div className={styles.thumbnail}></div>
-            <div className={styles.info}>
-              <h3>Título do Álbum/Playlist</h3>
-              <p>Informações adicionais</p>
-            </div>
-            <button className={styles.more_options}>&#8942;</button>
-          </article>
           
+            {resultList.map((result) => (
+              <article className={styles.result_item}>
+                <div className={styles.thumbnail}></div>
+                <div className={styles.info}>
+                  <h3>{result.name}</h3>
+                  <p>Informações adicionais</p>
+                </div>
+                <button className={styles.more_options}>&#8942;</button>
+              </article>
+            ))}
           
-          <article className={styles.result_item}>
-            <div className={styles.thumbnail}></div>
-            <div className={styles.info}>
-              <h3>Título do Álbum/Playlist</h3>
-              <p>Informações adicionais</p>
-            </div>
-            <button className={styles.more_options}>&#8942;</button>
-          </article>
-          
-          
-          <article className={styles.result_item}>
-            <div className={styles.thumbnail}></div>
-            <div className={styles.info}>
-              <h3>Título do Álbum/Playlist</h3>
-              <p>Informações adicionais</p>
-            </div>
-            <button className={styles.more_options}>&#8942;</button>
-          </article>
         </section>
       </main>
   );
